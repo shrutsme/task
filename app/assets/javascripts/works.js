aed9c32df4536dd822395f9e1b410c7e90b733ef
shrutsme <shruti.gautam.dce@gmail.com>
2015-04-07 00:19:50 -0700

    added id in li tags.

--- app/assets/javascripts/works.js
+++ app/assets/javascripts/works.js
@@ -3,14 +3,15 @@ $(document).ready(function() {
   $("#addTask").keyup(function (e) {
       if (e.keyCode == 13) {
         var name_tsk = document.getElementById("addTask").value;
-        $("#sortable").prepend('<li>'+name_tsk+'</li>');
           $.ajax({
               type: 'POST',
+              url:'../works',
               dataType : 'json',
               data: {work:{ name: name_tsk }}
             })
           .done(function() {
             alert( "Handler for .change() called.");
+            $("#sortable").prepend('<li class="ui-state-default">'+name_tsk+'<button type="button" class="btn btn-default btn-xs pull-right" aria-label="Delete"><i class="fa fa-trash-o "></i></button></li>');
           })
           .fail(function(XMLHttpRequest, textStatus, errorThrown) {
             console.log(errorThrown);
